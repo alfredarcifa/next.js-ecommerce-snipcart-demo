084841027933767dbef01fd7e4650ba2eb07a4f9
Alfred Arcifa <alfred.arcifa@gmail.com>
2023-02-03 16:19:36 +0100

    Update index.js
    Mods API Key Snipcart

--- pages/index.js
+++ pages/index.js
@@ -81,7 +81,7 @@ export default function Home({ subscription }) {
         id="snipcart"
         data-api-key={
           process.env.NEXT_PUBLIC_SNIPCART_API_KEY ||
-          'OWE3MmZmMjQtNTk3Yi00OThhLWEwMmUtZDY4ZWM4NzIwYzZiNjM2NjM0Mzc1NzE0MTUwNzI1'
+          'OWUzZjUxMTMtYzc5Ni00YzViLTk1OWYtZjE1NGY1OWFhM2JjNjM4MTEwMzM5NTA2OTcwNDI5'
         }
         hidden
       ></div>
